eb7b8f90ff97cf899b680b7db054e6ae316d7c1e
liuzhengkun666@163.com <liuzhengkun666@163.com>
2020-09-07 14:57:36 +0800

    zookeeper服务注册

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PayMentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PayMentMain8001.java
@@ -3,12 +3,14 @@ package com.atguigu.springcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.atguigu.springcloud.mapper")
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class PayMentMain8001 {
 
     public static void main(String[] args) {
