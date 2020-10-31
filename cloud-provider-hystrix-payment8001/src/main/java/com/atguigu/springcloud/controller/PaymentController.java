909b6df0baeda5596ecffe313f92d0ea0b24690d
liuzhengkun666@163.com <liuzhengkun666@163.com>
2020-10-31 09:44:50 +0800

    hystrix熔断

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -17,16 +17,24 @@ public class PaymentController {
     @Value("${server.port}")
     private String serverPort;
 
-    @GetMapping("/payemnt/hystrix/ok/{id}")
+    @GetMapping("/payment/hystrix/ok/{id}")
     public String paymentInfo_OK(@PathVariable("id") Integer id){
         String result = paymentService.paymentInfo_OK(id);
         log.info("===========result:" + result);
         return result;
     }
 
-    @GetMapping("/payemnt/hystrix/timeout/{id}")
+    @GetMapping("/payment/hystrix/timeout/{id}")
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
         String result = paymentService.paymentInfo_TimeOut(id);
+        log.info("===========result:" + result);
+        return result;
+    }
+
+    //服务熔断
+    @GetMapping("/payment/circuit/{id}")
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id){
+        String result = paymentService.paymentCircuitBreaker(id);
         log.info("===========result:" + result);
         return result;
     }
